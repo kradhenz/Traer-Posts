b26af7e1dfdfbd2f53a8d0ee3b082a66ff393502
kradhenz <tomas25498@gmail.com>
2024-02-27 01:49:27 -0300

    :bookmark: code with requirements developed :construction:

--- assets/js/desafioI.js
+++ assets/js/desafioI.js
@@ -1,15 +1,35 @@
-let datos = async function cargarDatos() {
+// async function to get posts from the API
+async function getPosts() {
+    // try-catch to manage errors
     try {
-        let respuesta = await fetch("https://jsonplaceholder.typicode.com/posts");
-        let datos = await respuesta.json();
+        // Make a request to the API to fetch the posts
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        let data = await response.json(); // Convert the response to JSON format
+        showPosts(data); // call function to show posts in HTML
     } catch (e) {
-        console.log(e)
+        console.log(e) // error message
     }
 }
-datos();
+// Function to display the posts in the HTML
+function showPosts(posts) {
+    let postData = document.getElementById("post-data"); // catch container element from HTML
+    let ul = document.createElement("ul"); // declare type of tag to create in HTML
 
-//crear una funcion que reciba el elemento html
+    posts.forEach(post => { // loop to create a new li and p element
+        // to create titles
+        let title = document.createElement("li"); // declare type of tag to create in HTML
+        title.textContent = post.title; // add title to element
+        ul.appendChild(title); // add title to ul element
 
+        // to create body
+        let body = document.createElement("p"); // declare type of tag to create in HTML
+        body.textContent = post.body; // add body to element
+        ul.appendChild(body); // add body to ul element
+    });
+    
+    postData.innerHTML = ""; // clean container before adding new posts
+    postData.appendChild(ul); // add posts list to the container
+}
 
-//insertarle de manera dinamica los datos
-
+// Call function to get posts using event click from button by id
+btn.addEventListener("click", getPosts);
